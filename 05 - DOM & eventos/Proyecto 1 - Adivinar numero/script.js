5946bfb67900a180be8d773dbd059dcc5bfa41c0
SantiagoLucia <santiluciaprivi@gmail.com>
2022-11-15 23:05:53 -0300

    05 - DOM & eventos/Proyecto 1 - Adivinar numero/script.js

--- 05 - DOM & eventos/Proyecto 1 - Adivinar numero/script.js
+++ 05 - DOM & eventos/Proyecto 1 - Adivinar numero/script.js
@@ -1,66 +1,55 @@
 'use strict';
 
-/*
-document.querySelector('.message').textContent = '🎉 Correct number!';
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-document.querySelector('.guess').value = 15;
-*/
-
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-let highScore = 0;
+let highscore = 0;
 
-const updateOnFail = function (message) {
-  if (score > 1) {
-    document.querySelector('.message').textContent = message;
-    score--;
-    document.querySelector('.score').textContent = score;
-  } else {
-    document.querySelector('.message').textContent = '💥 You lost the game!';
-    document.querySelector('.score').textContent = 0;
-  }
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
 };
 
-//handler again button
-document.querySelector('.again').addEventListener('click', function () {
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
-  score = 20;
-  document.querySelector('.number').textContent = '?';
-  document.querySelector('.message').textContent = 'Start guessing...';
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.guess').value = '';
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
-});
-
-// Handler check button
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   // When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No number!';
+    displayMessage('⛔️ No number!');
 
     // When player wins
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 Correct number!';
+    displayMessage('🎉 Correct Number!');
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
-    if (score > highScore) {
-      highScore = score;
-      document.querySelector('.highscore').textContent = highScore;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
     }
 
-    // When guess is too high
-  } else if (guess > secretNumber) {
-    updateOnFail('📈 Too high!');
-
-    // When guess is too slow
-  } else if (guess < secretNumber) {
-    updateOnFail('📉 Too low!');
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('💥 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
